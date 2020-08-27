f379aec4e1fc1f3c856e75b5d63ab64a19220519
dorinzrihen <zrihend@gmail.com>
2020-08-27 17:35:01 +0300

    event listener tools

--- logic.js
+++ logic.js
@@ -37,6 +37,8 @@ const gameHight = 90;
 const calcCol = gameHight/5;
 const calcRow = gameWidth/2;
 
+const floorRange = 3;
+
 function setGameBackground(){
     //every div is 2vw 5vh.
     const gameMatrix = [];
@@ -52,28 +54,8 @@ function setGameBackground(){
     }
 }
 
-function setCloud(){
-    let cloudStartCol = Math.floor(Math.random() * 10)+1; 
-    let cloudStartRow = Math.floor(Math.random() * 10)+1; 
-    const cloud = [[0,0,1,1,0,0],
-                [1,1,1,1,1,1],
-                [0,0,0,1,1,1],
-                [0,0,0,0,0,0]]
-    for(i = 0; i<cloud.length; i++ ){
-        let row = cloudStartRow; 
-        for(j=0; j<6; j++){
-            let myDiv = gameObject.get(`${cloudStartCol},${row}`);
-            if(cloud[i][j] === 1){
-                myDiv.addDiv.classList.remove('box-sky');
-                myDiv.addDiv.classList.add('box-cloud');
-            }
-            row++;
-        }
-        row = cloudStartRow;
-        cloudStartCol++;
-    }
-}
 
+//set floor 3 blocks
 function setFloor(){
     for(i = 17; i>=15; i-- ){
         for(j=0; j<20; j++){
@@ -90,6 +72,18 @@ function setFloor(){
     }
 }
 
+//set cloud to the game
+function setCloud(){
+    let cloudStartCol = Math.floor(Math.random() * 10)+1; 
+    let cloudStartRow = Math.floor(Math.random() * 10)+1; 
+    const cloud = [[0,0,1,1,0,0],
+                [1,1,1,1,1,1],
+                [0,0,0,1,1,1],
+                [0,0,0,0,0,0]];
+    setElement(cloudStartCol,cloudStartRow,cloud,'box-cloud');
+}
+
+//set tree with random row
 function setTree(){
     let treeStartCol = 6;
     const treeRandom = Math.floor(Math.random() * 10)+1;
@@ -102,34 +96,88 @@ function setTree(){
                 [0,0,0,2,0,0],
                 [0,0,0,2,0,0],
                 [0,0,0,2,0,0]];
-    for(i = 0; i<tree.length; i++ ){
-        let treeStartRow = treeRandom;
-        for(j=0; j<6; j++){
-            let myDiv = gameObject.get(`${treeStartCol},${treeStartRow}`);
-            
-            if(tree[i][j] === 1){
-                myDiv.addDiv.classList.remove('box-sky');
-                myDiv.addDiv.classList.add('box-grass');
-            }
-            else if(tree[i][j] === 2){
-                myDiv.addDiv.classList.remove('box-sky');
-                myDiv.addDiv.classList.add('box-tree');
-            }
-            treeStartRow++;
-        }
-        treeStartCol++;
-    }
-
+    setElement(treeStartCol,treeRandom,tree,'box-grass','box-tree');
 }
 
+
+//set rock
+function setRock(){
+    let rockStartCol = calcCol - 6;
+    let rockStartRow = Math.floor(Math.random() * 10)+1; 
+    const rock = [[1,1],[1,1],[1,1]]
+    setElement(rockStartCol,rockStartRow,rock,'box-rock');
+}
+
+
+function setBush(){
+    let bushStartCol = calcCol - 5;
+    let bushStartRow = Math.floor(Math.random() * 10)+1; 
+    const bush = [[1,1,1],[1,1,1]];
+    setElement(bushStartCol,bushStartRow,bush,'box-grass');
+}
+
+function setElement(startCol,startRow,arr,elementClass,elementClass2){
+    let startColNum = startCol;
+    let startRowNum = startRow;
+
+    for(i = 0; i< arr.length; i++ ){
+        let row = startRowNum; 
+        for(j=0; j< arr[0].length; j++){
+            let myDiv = gameObject.get(`${startColNum},${row}`);
+            if(arr[i][j] === 1){
+                myDiv.addDiv.classList.remove('box-sky');
+                myDiv.addDiv.classList.add(elementClass);
+            }
+            else if(arr[i][j] === 2){
+                myDiv.addDiv.classList.remove('box-sky');
+                myDiv.addDiv.classList.add(elementClass2);
+            }
+            row++;
+        }
+        row = startRowNum;
+        startColNum++;
+    }
+}
 
 setGameBackground();
 setCloud();
 setCloud();
 setFloor();
 setTree();
+setRock();
+setBush();
 
-//const myDiv = gameObject.get('4,5');
+
+
+// tools
+const myTools = document.querySelector('.weapon-options');
+const toolMap = new Map();
+let currentTool;
+let currentElement;
+
+function createTool(name,picture){
+    const myDivTool = document.createElement('div');
+    let toolImg = `<img class='tool-pic' src="/imgs/${picture}" alt="${name}">`
+    myDivTool.classList.add("weapon-border");
+
+    myDivTool.innerHTML = toolImg;
+    myTools.appendChild(myDivTool);
+}
+
+
+createTool('axe','axe.png');
+createTool('pickaxe','pickaxe.png');
+createTool('shovel','shovel.png');
+//add event listeners
+
+
+const toolEvent = document.querySelectorAll('.weapon-border');
+for(i=0; i<toolEvent.length;i++){
+    toolEvent[i].addEventListener('click',function(element){
+        currentTool=element;
+        console.log(currentTool)
+    });
+}
 
 
 
