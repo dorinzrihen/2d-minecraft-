a770261ebeadbc6228b8055053bdca1ed84401a6
dorinzrihen <zrihend@gmail.com>
2020-08-27 13:13:53 +0300

    tree and floor

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 
-//main
+//start game
 
 function main(){
     const minecraftStat = document.createElement('div');
@@ -28,35 +28,79 @@ function main(){
 //main();
 
 const divContainer = document.querySelector('.game-container');
+const gameObject = new Map();
+
+
 function setGameBackground(){
     const gameMatrix = [];
-    
     for(i = 0;i<18;i++){
         gameMatrix[i] = [];
         for(j=0; j<20; j++){
             const addDiv = document.createElement('div');
             addDiv.classList.add("box");
-            addDiv.classList.add("box-sky")
+            addDiv.classList.add("box-sky");
             divContainer.appendChild(addDiv);
+            gameObject.set(`${i},${j}`,{addDiv}); 
         }
     }
-    return gameMatrix;
 }
 
-function setFloor(matrix){
-    for(i = matrix.length ; i>=matrix.length-4; i-- ){
+function setFloor(){
+    for(i = 17; i>=15; i-- ){
         for(j=0; j<20; j++){
-            const addDiv = divContainer.querySelector('div');
-            addDiv.classList.remove("box-sky")
-            addDiv.classList.add("box-floor");
-            divContainer.appendChild(addDiv);
+            const test = gameObject.get(`${i},${j}`)
+            if(i == 15){
+                test.addDiv.classList.remove('box-sky')
+                test.addDiv.classList.add('box-half-floor')
+            }
+            else{
+                test.addDiv.classList.remove('box-sky')
+                test.addDiv.classList.add('box-floor')
+            }
         }
     }
 }
 
+function setTree(){
+    let treeStartCol = 6;
+    const tree = [[0,0,1,1,0,0],
+                [1,1,1,1,1,1],
+                [1,1,1,1,1,1],
+                [1,1,1,1,1,0],
+                [0,0,1,1,0,0],
+                [0,0,0,2,0,0],
+                [0,0,0,2,0,0],
+                [0,0,0,2,0,0],
+                [0,0,0,2,0,0]];
+    console.log(tree.length);
+    for(i = 0; i<tree.length; i++ ){
+        let treeStartRow = 3;
+        for(j=0; j<6; j++){
+            let test = gameObject.get(`${treeStartCol},${treeStartRow}`);
+            
+            if(tree[i][j] === 1){
+                test.addDiv.classList.remove('box-sky');
+                test.addDiv.classList.add('box-grass');
+            }
+            else if(tree[i][j] === 2){
+                test.addDiv.classList.remove('box-sky');
+                test.addDiv.classList.add('box-tree');
+            }
+            treeStartRow++;
+        }
+        treeStartCol++;
+    }
 
-let gameMatrix = setGameBackground();
-console.log(gameMatrix.length);
-setFloor(gameMatrix);
+}
+
+
+setGameBackground();
+setFloor();
+setTree();
+
+//const test = gameObject.get('4,5');
+
+
+
 
 
